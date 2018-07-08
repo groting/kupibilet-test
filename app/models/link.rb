110762a25dd0433a19a276a2920952ac418a46a3
groting <groting24@gmail.com>
2018-07-08 23:42:17 +0300

    Rewrite UrlValidator class from link model

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,17 +2,22 @@ require 'uri'
 
 class UrlValidator < ActiveModel::Validator
   def validate(record)
-    begin
-      uri= URI.parse(record.long_url)
-      unless uri.kind_of?(URI::HTTP) || uri.kind_of?(URI::HTTPS)
-        record.errors[:base] << "This url is not valid"
-      end
-    rescue URI::InvalidURIError
-      record.errors[:base] << "This url is not valid"
-    end
+    uri = URI.parse(record.long_url)
+    mark_url_as_invalid(record) unless http_uri?(uri)
+  rescue URI::InvalidURIError
+    mark_url_as_invalid(record)
+  end
+
+  private
+
+  def http_uri?(uri)
+    uri.kind_of?(URI::HTTP) || uri.kind_of?(URI::HTTPS)
+  end
+
+  def mark_url_as_invalid(record)
+    record.errors[:base] << "This url is not valid"
   end
 end
-
 
 class Link < ApplicationRecord
   validates_with UrlValidator
